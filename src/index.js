1541b2586a01d2cefca68fdc4a125ca725c743d3
Moniem-JamSam <abdullmonim.jamsam@gmail.com>
2022-04-05 05:07:43 +0300

    hi

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+
+import i18n from "i18next";
+import { useTranslation } from "react-i18next";
+
 import App from './App';
 
 
